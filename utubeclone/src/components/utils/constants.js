f4fdfa6f8395f02c7f6f3ecafdcb6835f873cc62
Jayanth R <jayanthrnambiar@gmail.com>
2023-02-28 09:37:19 +0530

    final

--- utubeclone/src/components/utils/constants.js
+++ utubeclone/src/components/utils/constants.js
@@ -19,7 +19,6 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 //exporting the contents for categories bar
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
